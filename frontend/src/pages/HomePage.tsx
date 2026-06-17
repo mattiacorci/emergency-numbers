a0333f697ce3c9cf2b77ba5a4d7122b0e95728bd
Mattia Corciulo <mattia.corciulo@gmail.com>
2026-06-17 13:15:21 +0200

    added empty state

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -13,6 +13,10 @@ import { useReverseGeocode } from "@/features/location/useReverseGeocode";
 import { Button } from "@/components/ui/button"
 import { useEffect } from "react";
 import { Spinner } from "@/components/ui/spinner";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+import { ArrowUpRightIcon } from "lucide-react";
+import { IconCompass, IconFolderCode } from "@tabler/icons-react"
+
 
 export function HomePage() {
     const { coords, status, error, start } = useGeolocation();
@@ -26,23 +30,40 @@ export function HomePage() {
         <div>
             {/* <StatusBanner /> */}
 
-            {status === 'idle' && (
-            <Button size="lg" onClick={start} disabled={status === 'loading'}>
-                {status === 'loading' ? "Getting Location..." : "Get Location"}
-            </Button>
+            {(status === 'idle' || status === 'loading') && (
+                <Empty>
+                    <EmptyHeader>
+                        <EmptyMedia variant="icon">
+                            <IconCompass size={48} />
+                        </EmptyMedia>
+                        <EmptyTitle>No position yet</EmptyTitle>
+                        <EmptyDescription>
+                            In order to get information about your current position, you need to allow the browser to access your location.
+                        </EmptyDescription>
+                    </EmptyHeader>
+                    <EmptyContent className="flex-row justify-center gap-2">
+                        {status === 'idle' && (
+                            <Button size="lg" onClick={start}>
+                                Get Current Location
+                            </Button>
+                        )}
+
+                        {status === 'loading' && (
+                            <Button size="lg" disabled>
+                                <Spinner data-icon="inline-start" />
+                                Getting location...
+                            </Button>
+                        )}
+                    </EmptyContent>
+                </Empty>
+
             )}
 
-            {status === 'loading' && (
-            <Button size="lg" disabled>
-                <Spinner data-icon="inline-start" />
-                Getting location...
-            </Button>
-            )}
 
-            {status === 'success' && (
+            {status === 'success' && coords && (
                 <>
-                <CoordinatesDisplay coords={coords} />
-                <MapView coords={coords} />
+                    <CoordinatesDisplay coords={coords} />
+                    <MapView coords={coords} />
                 </>
             )}
 
