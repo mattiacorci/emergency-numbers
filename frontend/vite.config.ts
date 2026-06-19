a271dd8eee890f0ffdb1e2a4f4b87bd4fa37ddf0
Mattia Corciulo <mattia.corciulo@gmail.com>
2026-06-19 08:03:31 +0200

    refactor: update tsconfig and vite configuration for consistency and add vitest configuration. Also for keep running CI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import path from 'path'
 
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-
 })
