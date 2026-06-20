49e88e422c8bac54e802b0ead3281ff6064dfc2f
Mattia Corciulo <mattia.corciulo@gmail.com>
2026-06-20 20:52:19 +0200

    refactor: integrate dj_database_url for database configuration and update default settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST': {
-            'NAME': ':memory:'
-        },
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+
+DATABASES['default']['TEST'] = {'NAME': ':memory:'}
 
 
 # Password validation
