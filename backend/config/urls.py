bfe539ac9bfa22f71fac32dad7421713134cc0f2
Mattia Corciulo <mattia.corciulo@gmail.com>
2026-09-03 21:01:12 +0200

    fix: enable Swagger UI based on environment variable

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import include, path
 
@@ -29,3 +31,8 @@ urlpatterns = [
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
 
+if os.environ.get('SWAGGER_ENABLED', 'False') == 'True':
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    ]
